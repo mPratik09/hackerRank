a3a4ad125eff01df91a1092abeeedfe4cca50a72
Pratik Markad <pratik.markad1@gmail.com>
2025-09-10 00:57:21 +0530

    HackerRank: 'Java String Tokens' challenge using regex-based splitting

---
+++ hacker_rank/src/hackerrank/introduction/StringTokens.java
@@ -0,0 +1,27 @@
+package hackerrank.introduction;
+
+import java.util.Scanner;
+
+public class StringTokens
+{
+	
+	public static void main(String[] args)
+	{
+//		Scanner sc = new Scanner(System.in);
+		
+		String str = "He is a very very good boy, isn't he?";
+		
+		
+		String[] words = str.split("[^a-zA-Z]+");
+		
+		System.out.println(words.length);
+		
+		for(String word : words) {
+			System.out.println(word);
+		}
+		
+		
+//		sc.close();
+	}
+	
+}
