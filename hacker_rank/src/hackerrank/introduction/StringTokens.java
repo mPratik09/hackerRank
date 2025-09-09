2101eeca81e43f12c640f6cafbd3269b059083fd
Pratik Markad <pratik.markad1@gmail.com>
2025-09-10 01:03:00 +0530

    Check for empty or whitespace-only string to avoid incorrect token count

--- hacker_rank/src/hackerrank/introduction/StringTokens.java
+++ hacker_rank/src/hackerrank/introduction/StringTokens.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 
 public class StringTokens
 {
-	
+
 	public static void main(String[] args)
 	{
 //		Scanner sc = new Scanner(System.in);
-		
+
 		String str = "He is a very very good boy, isn't he?";
-		
-		
+
+		str = str.trim();
+		if (str.length() == 0)
+		{
+			System.out.println(0);
+			return;
+		}
+
 		String[] words = str.split("[^a-zA-Z]+");
-		
+
 		System.out.println(words.length);
-		
-		for(String word : words) {
+		for (String word : words)
+		{
 			System.out.println(word);
 		}
-		
-		
+
 //		sc.close();
 	}
-	
+
 }
